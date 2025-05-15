28fec024f880b65f8daf963cc61450f2cd211c79
David Moriez <capmaravilla@gmail.com>
2025-05-15 02:48:31 +0200

    acabado lo de pyratilla

--- prueba.py
+++ prueba.py
@@ -2,171 +2,123 @@ from os import system
 
 system("cls")
 
+from random import randrange
 
-class Date:
-    days_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    months_names = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ]
 
-    def __init__(self, day=1, month=1, year=1):
-        self.day = day
-        self.month = month
-        self.year = year
+class Pirate:
+    boat_life = 200
 
-    @staticmethod
-    def zero_filler(n, l):
-        n = str(n)
-        r = l - len(n)
-        if r == 0:
-            return n
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
+
+    def attack(self, enemy):
+        if self.weapon.scope == "short" and enemy.position == "far":
+            print("No puedes atacar con ese arma desde esa posicion!")
+            return "Attack!!"
         else:
-            for _ in range(r):
-                n = f"0{n}"
-        return n
-
-    def __str__(self):
-        return f"{self.zero_filler(self.day,2)}/{self.zero_filler(self.month,2)}/{self.zero_filler(self.year,4)}"
-
-    def isLeap(self):
-        if self.year % 4 == 0:
-            if self.year % 400 == 0:
-                return True
-            elif self.year % 100 == 0:
-                return False
-            return True
-        return False
-
-    def totalMonthDays(self):
-        if self.month > 12 or self.month < 1:
-            return "Introduce primero una fecha valida!"
-        if self.month == 2 and self.isLeap == True:
-            return 29
-        else:
-            return self.days_month[self.month - 1]
-
-    def validDate(self):
-        if self.month < 1 or self.month > 12:
-            return False
-        elif self.day < 1 or self.day > self.totalMonthDays():
-            return False
-        return True
-
-    @property
-    def monthName(self):
-        return self.months_names[self.month - 1]
-
-    @staticmethod
-    def areEqual(a, b):
-        if a.year == b.year and a.month == b.month and a.day == b.day:
-            return "La fechas son iguales"
-        return "Las fechas no son iguales"
-
-    @staticmethod
-    def isLater(a, b):
-        if a.year != b.year:
-            if a.year > b.year:
-                return a
-            return b
-        elif a.month != b.month:
-            if a.month > b.month:
-                return a
-            return b
-        elif a.day != b.day:
-            if a.day > b.day:
-                return a
-            return b
-        else:
-            Date.areEqual(a, b)
-
-    @staticmethod
-    def isPrevious(a, b):
-        if Date.isLater(a, b) == a:
-            return b
-        return a
+            enemy.health -= self.weapon.damage
 
     @classmethod
-    def firstDayOfTheYear(cls, year):
-        return cls(1, 1, year)
+    def hit(cls, damage):
+        cls.boat_life -= damage
 
-    @classmethod
-    def lastDayOfTheYear(cls, year):
-        return cls(31, 12, year)
 
-    def plusDay(self):
-        if self.day < self.days_month[self.month - 1]:
-            self.day += 1
-            return self.__str__()
-        elif self.month < 12:
-            self.day = 1
-            self.month += 1
-            return self.__str__()
-        else:
-            self.day = 1
-            self.month = 1
-            self.year += 1
-            return self.__str__()
+class Enemy:
 
-    def minusDay(self):
-        if self.day > 1:
-            self.day -= 1
-            return self.__str__()
-        elif self.month > 1:
-            self.month -= 1
-            self.day = self.days_month[self.month - 1]
-            return self.__str__()
-        else:
-            self.day = self.days_month[self.month - 1]
-            self.month = 12
-            self.year -= 1
-            return self.__str__()
+    def __init__(self, name, health, position, min_damage, max_damage):
+        self.name = name
+        self.health = health
+        self.position = position
+        self.min_damage = min_damage
+        self.max_damage = max_damage
 
-    @classmethod
-    def copy(cls, sample):
-        return cls(sample.day, sample.month, sample.year)
+    def attack(self, pirate):
+        damage = randrange(self.min_damage, self.max_damage)
+        Pirate.boat_life -= damage
+        print(f"¡Oh no! {self.name} ha atacado a {pirate.name}")
 
-    @staticmethod
-    def difference(a, b):
-        pos = Date.isLater(a, b)
-        pre = Date.isPrevious(a, b)
-        total_days = 0
+    def move(self):
+        if self.position == "far":
+            self.position = "near"
+        elif self.position == "near":
+            self.position = "far"
 
-        # Recorremos los años completos entre pre.year y pos.year
-        for year in range(pre.year, pos.year + 1):
-            temp_date = Date(1, 1, year)
-            if temp_date.isLeap():
-                total_days += 366  # Año bisiesto
+    def hit(self, damage):
+        self.health -= damage
+
+
+class Weapon:
+    def __init__(self, name, damage, scope):
+        self.name = name
+        self.damage = damage
+        self.scope = scope
+
+
+sword = Weapon("Espada", 3, "short")
+ax = Weapon("Hacha", 5, "short")
+bow = Weapon("Arco", 2, "short")
+
+torvellino1 = Enemy(
+    "Torvellino 1", health=25, position="near", min_damage=2, max_damage=5
+)
+torvellino2 = Enemy(
+    "Torvellino 2", health=25, position="near", min_damage=4, max_damage=8
+)
+torvellino3 = Enemy(
+    "Torvellino 3", health=25, position="far", min_damage=3, max_damage=5
+)
+
+torvellino4 = Enemy(
+    "Torvellino 4", health=25, position="far", min_damage=6, max_damage=9
+)
+
+pyratilla = Pirate("Pyratilla", sword)
+pym = Pirate("Pym", bow)
+pyerce = Pirate("Pyerce", ax)
+
+pyrates = [pyratilla, pym, pyerce]
+enemies = [torvellino1, torvellino2, torvellino3, torvellino4]
+
+
+def restart_battle():
+    print("los enemigos ganan!!")
+    enemies = [torvellino1, torvellino2, torvellino3, torvellino4]
+    for e in enemies:
+        e.health = 25
+    Pirate.boat_life = 200
+    batalla()
+
+
+def batalla():
+    print("Comienza la batalla!!")
+
+    while len(enemies) > 0:
+        print(Pirate.boat_life)
+        print(f"Quedan {len(enemies)} enemigos")
+        if Pirate.boat_life < 1:
+            print("Reiniciamos, los pyrates han perdido")
+            restart_battle()
+
+        for pyrate in pyrates:
+            if enemies[0].health < 1:
+                enemies.remove(enemies[0])
+                print("Enemigo muerto")
+            if len(enemies) == 0:
+                print("La victoria es de los Pyrates!!!")
+                break
             else:
-                total_days += 365  # Año no bisiesto
+                enemyTarget = enemies[0]
+                print("salud", enemyTarget.health)
+                pyrate.attack(enemyTarget)
 
-        # Restamos los días del año inicial antes de la fecha 'pre'
-        temp_pre = Date(1, 1, pre.year)
-        for month in range(1, pre.month):
-            temp_pre.month = month
-            total_days -= temp_pre.totalMonthDays()
-        total_days -= pre.day - 1
-
-        # Restamos los días del año final después de la fecha 'pos'
-        temp_pos = Date(1, 1, pos.year)
-        for month in range(pos.month + 1, 13):
-            temp_pos.month = month
-            total_days -= temp_pos.totalMonthDays()
-        total_days -= temp_pos.totalMonthDays() - pos.day
-
-        return total_days
+        for enemy in enemies:
+            al = randrange(0, 2)
+            if al == 1:
+                enemy.move()
+            else:
+                pyrateTarget = pyrates[randrange(0, len(pyrates))]
+                enemy.attack(pyrateTarget)
 
 
-a = Date(6, 5, 1976)
-b = Date(1, 1, 2025)
-print(Date.difference(a, b))
+batalla()
